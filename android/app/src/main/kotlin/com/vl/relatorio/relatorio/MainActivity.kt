3b451bcedc1cba8d4ac1a376b2b9f4a4cdd83935
Brunohfc <bhenrick8@gmail.com>
2025-02-15 17:17:23 -0300

    initial commit and appbar configuration begin

---
+++ android/app/src/main/kotlin/com/vl/relatorio/relatorio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vl.relatorio.relatorio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/app_bar.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+class MyAppBarWidget extends StatefulWidget{
+  const MyAppBarWidget({super.key});
+
+  @override
+  State<MyAppBarWidget> createState() => _MyAppBarWidgetState();
+}
+
+class _MyAppBarWidgetState extends State<MyAppBarWidget>{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(52, 35, 156, 109),
+            //#347F7A
+          title: Row(
+            children:<Widget> [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Image.asset("assets/images/vale.png")
+              ),
+              Center(
+                  child: Text("Criar Relatório")
+              )
+
+            ],
+          ),
+          centerTitle: true,
+          
+          ),
+
+        ),
+
+    );
+  }
+}
