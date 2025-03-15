815e2d38104bfcca32e7aa1bdf5cbcd1df5ebc6e
bruninho <bhenrick8@gmail.com>
2025-03-15 13:19:14 -0300

    inclusao da funcionalidade para adcionar equipamento preventiva

--- lib/screens/forms.dart
+++ lib/screens/forms.dart
@@ -1,36 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:relatorio/screens/preditiva.dart';
 
-class Forms extends StatefulWidget{
-  const Forms();
 
+import 'date_picker.dart';
+
+class Forms extends StatefulWidget {
+  const Forms({super.key});
 
   @override
   State<Forms> createState() => _FormsState();
 }
 
+class _FormsState extends State<Forms> {
+  //final controller = BoardDateTimeController();
 
-class _FormsState extends State<Forms>{
+  DateTime date = DateTime.now();
 
-    @override
-    Widget build(BuildContext context) {
-      return Form(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Nome do Supervisor"),
-                )
-
-              ],
-            ),
-
-
-          )
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Padding(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: "Nome do Supervisor"),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: "Nome do Técnico"),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Container(),
+          DatePickerScreen(),
+          PreventiveVehicle()
+        ],
+      ),
+    ));
+  }
 }
-
-
-
